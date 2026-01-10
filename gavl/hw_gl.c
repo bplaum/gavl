57a0427d9deb4ffb5df1aec3d3a4bbc11b48944e
Burkhard Plaum <burkhardplaum@gmail.com>
2026-01-10 16:10:32 +0100

    Fix texture clamping

--- gavl/hw_gl.c
+++ gavl/hw_gl.c
@@ -170,8 +170,8 @@ gavl_video_frame_t * gavl_gl_create_frame(const gavl_video_format_t * fmt)
     glBindTexture(GL_TEXTURE_2D, info->textures[i]);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     gavl_gl_flush_errors();
 
